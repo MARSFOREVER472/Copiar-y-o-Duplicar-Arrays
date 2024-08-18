1bbb3504a8cc8690f5a0994cfd7a50f66915fb9c
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-08-17 23:41:24 -0400

    Comienzo del proyecto!!!

---
+++ ArchivosEjecutables/script.js
@@ -0,0 +1,25 @@
+// Cómo copiar y/o duplicar Arrays:</h1>
+    
+// Método 1: La forma más primitiva (no recomendado)
+
+// changed the original array:
+
+let arr = ['london', 'Madrid', 'Paris']; // Arreglo principal.
+
+let arr2 = arr; // Arreglo secundario.
+
+console.log('arr2 Array:', arr2);
+
+console.log('arr Array:', arr);
+
+// arr2.push('Sao Paulo');
+
+// console.log('***New City Added***');
+
+// console.log(arr2);
+
+// console.log(arr);
+
+// We have added an item to arr2
+
+// But, the original array(arr) got modified too
